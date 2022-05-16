91e71cb887e3c09ffeced1e390731477758cbc12
Fenvrir <dragonofhokage@gmail.com>
2022-05-16 18:29:33 +0700

    add download functionality

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import { Header, Main, Footer } from './components'
 import { useReactToPrint } from 'react-to-print';
 import { useRef } from 'react';
+let html2pdf = require('html2pdf.js');
+
+let opt = {
+  margin: 1,
+  filename: 'My-CV.pdf',
+  image: { type: 'jpeg', quality: 0.98 },
+  html2canvas: { scale: 2 },
+  jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' }
+};
 
 function App() {
   const componentRef = useRef();
+
   const handlePrintClick = useReactToPrint({
     content: () => componentRef.current,
   });
+
+  const handleDownloadClick = () => {
+    html2pdf().from(componentRef.current).set(opt).save()
+  }
 
 
   return (
     <div className='App'>
       <div className='wrapper'>
-        <Header onClick={handlePrintClick} />
+        <Header onClickPrint={handlePrintClick} onClickDownload={handleDownloadClick} />
         <Main className='Main' inputRef={componentRef} />
         <Footer />
       </div>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,51 +1,66 @@
-import React from "react";
 import "./Header.scss";
-import { PrinterIcon } from "@heroicons/react/solid";
+import { PrinterIcon, DownloadIcon } from "@heroicons/react/solid";
 import styled from "styled-components";
+
 
 const Container = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
   margin-top: 0.75rem;
-  div {
-    font-size: 1.5rem;
+  span {
     font-weight: 700;
+    font-size: 1.5rem;
     margin-left: 0.5rem;
   }
- button{
-   border: none;
-   background-color: #fff;
-   cursor: pointer;
-   font-size: 1.3rem;
-   font-weight: bold;
-   &:hover{
-    background-color: #f3f3f3;
-    border-radius: 20px;
-    padding: 0 4px 0 4px;
-   }
- }
-  .icon{
+  .Header_buttons {
+    display: flex;
+    gap: 10px;
+    box-sizing: border-box;
+    margin: 0 10px;
+    button {
+      border: none;
+      background-color: #fff;
+      cursor: pointer;
+      font-size: 1.3rem;
+      font-weight: bold;
+      &:hover {
+        background-color: #f3f3f3;
+        border-radius: 20px;
+        font-size: 1.4rem;
+      }
+    }
+  }
+
+  .icon {
     height: 1.5rem;
     width: 1.5rem;
     margin: 0 5px -5px 0;
-
   }
 `;
 
-const Header = ({ onClick }) => {
+const Header = ({ onClickPrint, onClickDownload }) => {
   return (
     <header className="Header">
       <Container>
         <div className="Header_wrapper">
           <span>CV Builder</span>
         </div>
-        <div>
+        <div></div>
+        <div className="Header_buttons">
+          <div>
+            <button type="button" onClick={onClickDownload}>
+              <DownloadIcon className="icon" aria-hidden="true" />
+              Download
+            </button>
+          </div>
+          <div>
+            <button type="button" onClick={() => onClickPrint()}>
+              <PrinterIcon className="icon" aria-hidden="true" />
+              Print
+            </button>
+          </div>
         </div>
-        <button type="button" onClick={() => onClick()}>
-          <PrinterIcon className="icon" aria-hidden="true" />
-          Print
-        </button>
       </Container>
     </header>
   );
